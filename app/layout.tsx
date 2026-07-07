ad51a0d00d80f099e711c46039394d0e7ec89ee4
Herbert Tamakloe <eliwebservices@gmail.com>
2026-07-07 16:08:44 +0000

    Hero image and about image chnaged

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,11 +33,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <Navbar /> */}
-        {/* <main className="pt-16">{children}</main> */}
-        <main className="">{children}</main>
-        {/* <Footer /> */}
-        {/* <ChatWidget /> */}
+        <Navbar />
+        <main className="pt-16">{children}</main>
+        <Footer />
+        <ChatWidget />
       {process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_GA_ID && (
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
       )}
